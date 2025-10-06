2c078b86ea78b14bceb0ef85e1543c403aa78a94
oliverlaslett <11660098+owlas@users.noreply.github.com>
2025-10-06 14:38:47 +0100

    update 0.2055.0

--- Formula/lightdash.rb
+++ Formula/lightdash.rb
@@ -1,17 +1,17 @@
 class Lightdash < Formula
   desc "CLI for the Lightdash BI platform"
   homepage "https://github.com/lightdash/lightdash"
-  version "0.2040.0"
+  version "0.2055.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/lightdash/lightdash/releases/download/0.2040.0/lightdash-cli-v0.2040.0-macos-arm64.tar.gz"
-      sha256 "b052a2943d4d1fee3e0eb8046d364b2296b97d11416bff792e85cc49c6c5e5b6"
+      url "https://github.com/lightdash/lightdash/releases/download/0.2055.0/lightdash-cli-v0.2055.0-macos-arm64.tar.gz"
+      sha256 "1e5a6c811acdc946f0f1a68c46898cd3bf7e3d5e01b89bae2f05f85d001c1933"
     end
     on_intel do
-      url "https://github.com/lightdash/lightdash/releases/download/0.2040.0/lightdash-cli-v0.2040.0-macos-x64.tar.gz"
-      sha256 "23128fb55b2d23bac6c5d8c33b47d3ed876d92f4a363498604c565c6378b8425"
+      url "https://github.com/lightdash/lightdash/releases/download/0.2055.0/lightdash-cli-v0.2055.0-macos-x64.tar.gz"
+      sha256 "43c2f5e74a0bc060e38b3bcf8f51c007f4aa2f9147b2a427761bbba0dc127b58"
     end
   end
 
